7e1813d292a47a6fdc0e7525c99c9c2679d6b149
Vincent <chenjuzen@gmail.com>
2017-05-20 11:32:26 +0800

    add testcase

---
+++ 137testcase.cs
@@ -0,0 +1,171 @@
+//simple statement and initialized
+int a;
+int b,c;
+double d=2.2,e=3.34;
+int a[2];
+int b[3][2];
+int c[2],d[3];
+int a[2]={3,2,3};
+int b[3][2]={2};
+int a=3;double c=2;int b[2][3]={2,3};
+bool g=true,e=false;
+char c='3';
+char c[2]={"df"};
+bool b[2]={true,false};
+//const
+const int a=3;
+const double b=2.3,c=2;
+const int a=2;const double b=32.3;const bool g=true;const char d = '2';
+//function
+int a;
+int main(){	
+}
+int cool(int a,double b,bool c,char d[2]){
+}
+int a;void co(){}
+int goo(int e[3][2][4])
+{
+	int a=3;
+}
+// expression check
+//int e[2+3/4]={5+3*5>4};
+//mix use
+int test(){
+a= 324 + 3;
+b = 1.2-3/4;
+c = 4*9%3;
+d=78 + 5 < 34;
+e = 67 - 3>=23;
+f = 87 <=1;
+g = 23 > 4;
+h = 7*(1+3);
+i = 8+(j++);
+j = k+l;
+k = a + (b<2)*(c>=d);
+l = -2;
+m = 4-(-2);
+n = g+!73;
+//gj++;
+//jg--;
+tmd[886+9487]=4&&2+gj++;
+//j+2;
+//8787>=33||23;
+b=cc(EMT);
+//function usage
+emt();
+eee(3.0+232/4.121*3.1415);
+bbc("haha",false,2,3.0);
+a = b( c() , d() );
+gg(max()+min()*abs(emt(!486+a)), c[7]);
+}
+
+int test_for()
+{
+	for(8&&3;3<12;9+1){
+		for(;;){}
+	}
+//	for(i=0;;) { a=2; } int EMT=9487; //No strict order	
+}
+
+void test_if()
+{
+	if(i==0){ int j=2,i=0; }
+	else{EMT=486;}
+	if(gg){
+		if(EMT<=486){}
+		else{}
+	}
+
+}
+void test_while()
+{
+	while(EMT<=486){EMT=EMT+1;}
+	do{}while(EMT==486);
+	while(7878){}
+	while(!gogogo){}
+
+}
+void test_bcr()
+{
+	for(;;)
+	{
+		break;
+	}
+	while(i<EMT)
+	{
+		
+		continue;
+	}
+	return EMT;
+	return EMT;
+}
+void test_switch()
+{
+	switch(EMT)
+	{
+		case 4:
+			break;
+		case 'd':
+		default:
+			EMT=486;
+			break;
+	}
+
+}
+int mix_use(double g[2][3],char c[3])
+{
+	int EMT=486;
+	int TT[5]={false,"cc",4.32,3};
+	//double max = max(3.1415926,TT);
+	double sci = 12.03E41;
+	for(;i<100;)
+	{
+		for(;;)
+		{
+			if(!(max>=100) ){
+				max = 486;
+			}
+			 
+		}
+		
+		if(EMT!=486) {continue;}
+		switch(EMT)
+		{
+			case 'd':
+			case 1:
+			case 486:
+
+				cc[5] = 5;
+				break;
+		}
+		printf("%d",abc);	//test std function
+		return EMT;
+	}
+}
+int test_self()	//remove for simple test
+{ 
+for(;i<10;i++){}
+for(;i<10&&j>3;i++,j++);
+for(;;)
+{
+	for(;;i--){
+		for(;j<12;);
+	}
+}
+while(QQ);
+if(GG){}
+else if(EMT>999){}
+
+}
+
+void test_error(){
+// int cc(){	int dd(){}  }//function nested detect
+// switch(EMT){case 486: char cc[5]={"12345"};} //define in switchcase
+// switch(EMT486){default: case 3: break;}	//default not at last
+// switch(ETTT){}	//no case inside
+// if(486){a=2; int EMT=9487;} //strict order
+// while(1){a=2; int EMT=9487;} //strict order
+// int GG(){	for(;;){} int ee(){} }//function nested detect
+// for(;test;){a=2; int EMT=9487;} //strict order
+// {a=2; int EMT=9487;} //strict order
+}
